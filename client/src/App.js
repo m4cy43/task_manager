00a5171925a9da8edb4235c646e912b9f688ed87
Andrey Miroshnichenko <andreymirosh02@gmail.com>
2022-08-12 15:49:28 +0300

    main page start

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,14 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route, useNavigate } from 'react-router-dom';
 import Header from './components/Header';
 import SignUp from './pages/SignUp';
 import LogIn from './pages/LogIn';
+import MainPage from './components/MainPage';
 
 function App() {
+
+  // const navigate=useNavigate();
+  // const navigateMainPage=()=>{navigate("/main")};
   return (
     <>
       <Router>
@@ -13,6 +17,7 @@ function App() {
           <Routes>
             <Route path="/signup" element={<SignUp />} />
             <Route path="/login" element={<LogIn />} />
+            {/* <Route path="/main" element={<MainPage />} /> */}
           </Routes>
         </div>
       </Router>
