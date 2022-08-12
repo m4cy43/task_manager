7585760233b7a55a4ea50125d2d808f8971b9f11
Andrey Miroshnichenko <andreymirosh02@gmail.com>
2022-08-12 13:57:05 +0300

    Finish SignUp form

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -6,41 +6,44 @@ import { Link, useNative } from "react-router-dom";
 
 function Header() {
     return (
-        <header className="task-manager_head-menu">
+        <div className="back-image">
+            <header className="task-manager_head-menu">
 
-            <div className="task-manager_head-navigation">
+                <div className="task-manager_head-navigation">
 
-                <h3 className="task-manager_name">SillyTask</h3>
+                    <h3 className="task-manager_name">SillyTask</h3>
 
-                <ul className="task-manager_links">
+                    <ul className="task-manager_links">
 
-                    <li className="task-manager_links-item">
-                        <Link to="#">
-                            <BsFillCalendarDateFill />
-                        </Link>
-                    </li>
-                    <li className="task-manager_links-item">
-                        <Link to="#">
-                            <HiOutlineMailOpen />
-                        </Link>
-                    </li>
-                    <li className="task-manager_links-item">
-                        <Link to="#">
-                            <BsSearch />
-                        </Link>
-                    </li>
-                    <li className="task-manager_links-item">
-                        <Link to="/login">Log In</Link>
-                    </li>
-                    <li className="task-manager_links-item">
-                        <Link to="/signup">Sign Up</Link>
-                    </li>
-                    
-                </ul>
-            </div>
+                        <li className="task-manager_links-item">
+                            <Link to="#">
+                                <BsFillCalendarDateFill />
+                            </Link>
+                        </li>
+                        <li className="task-manager_links-item">
+                            <Link to="#">
+                                <HiOutlineMailOpen />
+                            </Link>
+                        </li>
+                        <li className="task-manager_links-item">
+                            <Link to="#">
+                                <BsSearch />
+                            </Link>
+                        </li>
+                        <li className="task-manager_links-item">
+                            <Link to="/login">Log In</Link>
+                        </li>
+                        <li className="task-manager_links-item">
+                            <Link to="/signup">Sign Up</Link>
+                        </li>
 
+                    </ul>
 
-        </header>
+                </div>
+
+            </header>
+            
+        </div>
     )
 }
 
