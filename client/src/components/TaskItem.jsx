0ade426f1170805e7aae24f925ce0adeff17a6b5
Andrey Miroshnichenko <andreymirosh02@gmail.com>
2022-08-13 13:40:07 +0300

    a little addition to items

--- client/src/components/TaskItem.jsx
+++ client/src/components/TaskItem.jsx
@@ -5,6 +5,7 @@ function TaskItem({goal, deadline}) {
     return (
         <div className="main-page-tasks-item">
             <p className="main-page-tasks-item-deadline">{deadline}</p>
+            <div className="main-page-tasks-item-icon"><AiOutlineClose /></div>
             <h3 className="main-page-tasks-item-name">{goal.name}</h3>
             <p className="main-page-tasks-item-text">{goal.content}</p>
         </div>
